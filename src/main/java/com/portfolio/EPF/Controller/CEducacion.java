6f232d296c844cedc0e582a720ac84d5cb4170f9
edufonto22 <eduardofontora7@gmail.com>
2023-04-24 20:40:19 -0300

    se modifico el enlace del controller

--- src/main/java/com/portfolio/EPF/Controller/CEducacion.java
+++ src/main/java/com/portfolio/EPF/Controller/CEducacion.java
@@ -25,7 +25,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("educacion")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://backendfep.onrender.com")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;
--- src/main/java/com/portfolio/EPF/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/EPF/Controller/CExperiencia.java
@@ -25,7 +25,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://backendfep.onrender.com")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
