82db53c5bb7fa16680bbcc541d3482358ca1cff6
edufonto22 <eduardofontora7@gmail.com>
2023-06-11 21:47:07 -0300

    linkeando a firebase

--- src/main/java/com/portfolio/EPF/Controller/CEducacion.java
+++ src/main/java/com/portfolio/EPF/Controller/CEducacion.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("educacion")
-@CrossOrigin(origins = "https://backendfep.onrender.com")
+@CrossOrigin(origins = "https://frontendepf.web.app/")
 //@CrossOrigin(origins = "http://localhost:4200")
 public class CEducacion {
     @Autowired
