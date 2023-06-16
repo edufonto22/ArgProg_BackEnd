020ff2e03d1018549aabd2df8f99e270b7e7e057
edufonto22 <eduardofontora7@gmail.com>
2023-06-15 23:50:48 -0300

    se sacó / y se agregó la s

--- src/main/java/com/portfolio/EPF/Controller/PersonaController.java
+++ src/main/java/com/portfolio/EPF/Controller/PersonaController.java
@@ -22,8 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "https://frontendepf.web.app/")
-@RequestMapping("/persona")
+@CrossOrigin(origins = "https://frontendepf.web.app")
+@RequestMapping("/personas")
 public class PersonaController {
     @Autowired 
             ImpPersonaService personaService;
